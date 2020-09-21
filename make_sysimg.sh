0915c6de44ba3a259d8b39d45202ef20c7ed18ad
Alexander Barth <barth.alexander@gmail.com>
2020-09-21 13:18:46 +0200

    cpu_target="core2"

--- make_sysimg.sh
+++ make_sysimg.sh
@@ -7,5 +7,5 @@ run(`julia  --trace-compile=DIVAnd_trace_compile.jl DIVAnd_precompile_script.jl`
 PackageCompiler.create_sysimage(
     [:DIVAnd];
     sysimage_path="sysimg_DIVAnd.so",
-    #precompile_execution_file="make_sysimg_commands.jl")
+    cpu_target="core2",
     precompile_statements_file="DIVAnd_trace_compile.jl")
